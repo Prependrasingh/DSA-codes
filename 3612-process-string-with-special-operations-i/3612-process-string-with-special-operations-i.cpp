c0663cb51916ab7e8cac1268a1878f48c8de214c
Prependra singh <153770618+Prependrasingh@users.noreply.github.com>
2026-06-27 12:36:22 +0530

    Time: 12 ms (9.31%), Space: 39.6 MB (92.72%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string processStr(string s) {
+        string ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] >= 'a' && s[i] <= 'z') {
+                ans.push_back(s[i]);
+            } else if (s[i] == '*') {
+                if (!ans.empty()) {
+                    ans.pop_back();
+                }
+            } else if (s[i] == '#') {
+                ans = ans + ans;
+            } else {
+                reverse(ans.begin(), ans.end());
+            }
+        }
+        return ans;
+    }
+};
