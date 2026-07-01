187064f2fdc019aa773076b2ad7dbb714d80918d
Prependra singh <153770618+Prependrasingh@users.noreply.github.com>
2026-07-02 02:33:11 +0530

    Time: 0 ms (100%), Space: 312.2 MB (16.14%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -1,51 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
+        if (!head->next) return nullptr;
 
-        if(head == NULL || head->next == NULL){
-            return NULL;
+        ListNode* slow = head;
+        ListNode* fast = slow->next->next;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        int cnt = 1;
-        ListNode* temp = head;
-        ListNode* prev = nullptr;
-
-        while (temp->next != NULL) {
-            cnt++;
-            temp = temp->next;
-        }
-
-        temp = head;
-
-        int middle = 0;
-
-        while(middle != cnt/2){
-            prev = temp;
-            temp = temp->next;
-            middle++;
-        }
-
-        if(prev == NULL){
-            ListNode* newHead = head->next;
-            delete head;
-            return newHead;
-        }
-
-        prev->next = temp->next;
-        delete(temp);
+        slow->next = slow->next->next;
         return head;
-
-
-
     }
 };
