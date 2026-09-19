6de541e841930c5ba08c367385e15687e4510686
Prependra singh <153770618+Prependrasingh@users.noreply.github.com>
2026-09-19 13:16:01 +0530

    Time: 0 ms (100%), Space: 12.1 MB (12.51%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& v) {
-        sort(v.begin() , v.end());
-
-        string one = v[0];
-        string two = v[v.size() - 1];
-
+        sort(v.begin(), v.end());
         string ans = "";
 
-        for(int i = 0 ; i < min(one.length() , two.length()) ; i++){
-            if(one[i] != two[i]){
+        string s1 = v[0];
+        string s2 = v[v.size() - 1];
+
+        for (int i = 0; i < min(s1.size(), s2.size()); i++) {
+            if (s1[i] != s2[i]) {
                 break;
-            }
-            else{
-                ans += one[i];
+            } else {
+                ans.push_back(s1[i]);
             }
         }
-
         return ans;
     }
 };
